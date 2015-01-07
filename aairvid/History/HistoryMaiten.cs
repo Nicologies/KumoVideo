2adf9892063a75ff3efaa618782e87b4f5d5f741
Shanshan Du <nicoleflopy@gmail.com>
2015-01-07 19:41:25 +0800

    Ref #13: relocation files to history folder

--- aairvid/History/HistoryMaiten.cs
+++ aairvid/History/HistoryMaiten.cs
@@ -3,6 +3,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
+using libairvidproto.model;
 using HistoryContainer = System.Collections.Generic.Dictionary<string, aairvid.Model.HistoryItem>;
 
 namespace aairvid.Utils
@@ -12,35 +13,35 @@ namespace aairvid.Utils
         private static readonly string HISTORY_FILE_NAME = "./history.bin";
         private static readonly string HISTORY_FILE = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), HISTORY_FILE_NAME);
 
-        private static HistoryContainer _history = new HistoryContainer();
+        public static HistoryContainer HistoryItems = new HistoryContainer();
 
         public static void Load()
         {
-            if (_history.Count() == 0)
+            if (HistoryItems.Count() == 0)
             {
                 if (File.Exists(HISTORY_FILE))
                 {
                     using (var stream = File.OpenRead(HISTORY_FILE))
                     {
                         var fmt = new BinaryFormatter();
-                        _history = fmt.Deserialize(stream) as HistoryContainer;
+                        HistoryItems = fmt.Deserialize(stream) as HistoryContainer;
                     }
                 }
             }
 
             int maxHis = 200;
 
-            if (_history.Count() > maxHis)
+            if (HistoryItems.Count() > maxHis)
             {
-                var temp = _history.OrderBy(r => r.Value.LastPlayDate);
-                _history = temp.Skip(temp.Count() / 2).ToDictionary(r => r.Key, r => r.Value);
+                var temp = HistoryItems.OrderBy(r => r.Value.LastPlayDate);
+                HistoryItems = temp.Skip(temp.Count() / 2).ToDictionary(r => r.Key, r => r.Value);
             }
         }
 
-        public static void SaveLastPos(string vidBaseName, long pos)
+        public static void SaveLastPos(string vidBaseName, long pos, AirVidResource parent)
         {
             HistoryItem hisItem;
-            if (_history.TryGetValue(vidBaseName, out hisItem))
+            if (HistoryItems.TryGetValue(vidBaseName, out hisItem))
             {
                 hisItem.LastPlayDate = DateTime.Now;
                 hisItem.LastPosition = pos;
@@ -50,38 +51,34 @@ namespace aairvid.Utils
                 hisItem = new HistoryItem()
                 {
                     LastPosition = pos,
-                    LastPlayDate = DateTime.Now
+                    LastPlayDate = DateTime.Now,
+                    Server = parent.Server.Name,
+                    FolderPath = parent.GetPath(),
+                    FolderId = parent.Id,
                 };
-                _history.Add(vidBaseName, hisItem);
+                HistoryItems.Add(vidBaseName, hisItem);
             }
 
             using (var stream = File.OpenWrite(HISTORY_FILE))
             {
-                new BinaryFormatter().Serialize(stream, _history);
+                new BinaryFormatter().Serialize(stream, HistoryItems);
             }
         }
         public static long GetLastPos(string vidBaseName)
         {
             var item = GetLastPlayedInfo(vidBaseName);
-            if (item == null)
-            {
-                return 0;
-            }
-            return item.LastPosition;
+            return item == null ? 0 : item.LastPosition;
         }
 
         public static HistoryItem GetLastPlayedInfo(string vidBaseName)
         {
             HistoryItem hisItem;
-            if (_history.TryGetValue(vidBaseName, out hisItem))
+            if (HistoryItems.TryGetValue(vidBaseName, out hisItem))
             {
                 hisItem.LastPlayDate = DateTime.Now;
                 return hisItem;
             }
-            else
-            {
-                return null;
-            }
+            return null;
         }
     }
 }
